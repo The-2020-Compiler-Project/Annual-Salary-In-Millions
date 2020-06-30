3a93cc01236b0e5d9117877165af6de7c69824f1
amtoaer <b1361974998@gmail.com>
2020-07-01 00:25:36 +0800

    add grammar.cpp&header.h | remove outdated file

---
+++ 课设代码/grammar.cpp
@@ -0,0 +1,35 @@
+#include "header.h"
+// 对词法分析包装内函数的定义
+lexic_wrapper::lexic_wrapper()
+{
+    lex = new lexic();
+    isSuccess = lex->lexic_analyze();
+    position = lex->token_list.begin();
+}
+lexic_wrapper::~lexic_wrapper()
+{
+    delete lex;
+}
+token lexic_wrapper::getToken()
+{
+    if (position != lex->token_list.end()) {
+        token result = *position;
+        position++;
+        return result;
+    } else {
+        return token { "", 0 };
+    }
+}
+// 对语法分析类内函数的定义
+grammar::grammar()
+{
+    lexic = new lexic_wrapper();
+}
+grammar::~grammar()
+{
+    delete lexic;
+}
+void grammar::getToken()
+{
+    w = lexic->getToken();
+}
