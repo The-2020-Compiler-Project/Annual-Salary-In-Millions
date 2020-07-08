0324fdfd53e68f1cfd60cd20c18eaa8edf14447f
MengLong <1347518880@qq.com>
2020-07-08 10:51:00 +0800

    将目标代码生成和语义分析进行整合

--- 课设代码/main.cpp
+++ 课设代码/main.cpp
@@ -1,10 +1,26 @@
-#include "grammatical_analysis.cpp"
+#include "object_code_generate.cpp"
 
 
 int main()
 {
     grammar* test = new grammar();
     test->begin();
+
+    optimize_list=quaternion_list;
+
+    cout<<"生成的四元式为："<<endl;
+
+    test->print_quaternion_list();
+
+    cout<<endl;
+
+    fcode* fcode_test = new fcode();
+
+    fcode_test->to_code();
+
+    cout<<"生成的目标代码为："<<endl;
+    fcode_test->print_fcode_array();
+
     system("pause");
     return 0;
 }
